43ee54ca709329614785549e4a01f393d54d94a4
sd12key <166713967+sd12key@users.noreply.github.com>
2025-01-23 12:54:47 -0330

    Initial commit. Just hello-world code

---
+++ index.js
@@ -0,0 +1,9 @@
+const process = require("process");
+
+const APP_VERSION = "0.0.1";
+
+console.log(
+  "\nNode.js Command Line Password Generator (ver." + APP_VERSION + ")\n"
+);
+
+process.exit(0);
